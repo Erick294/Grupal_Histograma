ec208737f40dc7c55b16d66d79acb4cc66841261
Eric Sebastian <beowulfblackman@hotmail.com>
2023-08-08 02:44:00 +0000

    Gnu

---
+++ CMakeFiles/GrupalHistograma.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GrupalHistograma.dir/main.cpp.o"
+  "CMakeFiles/GrupalHistograma.dir/main.cpp.o.d"
+  "GrupalHistograma"
+  "GrupalHistograma.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GrupalHistograma.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
